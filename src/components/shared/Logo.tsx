fa3b16358fd734db1ec08af94f3d37f3c1fdddff
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-02-14 10:46:02 +0600

    Add: Footer

---
+++ src/components/shared/Logo.tsx
@@ -0,0 +1,12 @@
+import { SiElasticsearch } from "react-icons/si";
+
+const Logo = () => {
+  return (
+    <h1 className="text-[2wh] font-black flex items-center">
+      <SiElasticsearch size={34} className="text-purple-600 mr-3" />
+      GATJET WORLD
+    </h1>
+  );
+};
+
+export default Logo;
