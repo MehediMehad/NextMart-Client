534a2779a378760511bb823d2920a01c811e5c5e
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-02-27 22:07:31 +0600

    Update: Modify Sidebar

---
+++ src/app/(WithDashboardLayout)/user/dashboard/shop/category/page.tsx
@@ -0,0 +1,9 @@
+const ProductCategoryPage = () => {
+  return (
+    <div>
+      <h1>This is ProductCategoryPage component</h1>
+    </div>
+  );
+};
+
+export default ProductCategoryPage;
--- src/app/(WithDashboardLayout)/user/page.tsx
+++
@@ -1,9 +0,0 @@
-const UserHomePage = () => {
-  return (
-    <div>
-      <h1>This is UserHomePage component</h1>
-    </div>
-  );
-};
-
-export default UserHomePage;
